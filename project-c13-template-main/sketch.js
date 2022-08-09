9dff65499a1abdca74eccfb2760ee6ac104e32de
BrenoAnjos <108766558+BrenoAnjos@users.noreply.github.com>
2022-08-09 18:11:29 -0300

    .

--- project-c13-template-main/sketch.js
+++ project-c13-template-main/sketch.js
@@ -1,5 +1,6 @@
 var garden,rabbit;
 var gardenImg,rabbitImg;
+var random;
 
 function preload(){
   gardenImg = loadImage("garden.png");
@@ -21,6 +22,8 @@ garden.addImage(gardenImg);
 rabbit = createSprite(180,340,30,30);
 rabbit.scale =0.09;
 rabbit.addImage(rabbitImg);
+
+
 }
 
 
@@ -30,7 +33,50 @@ function draw() {
 
   edges= createEdgeSprites();
   rabbit.collide(edges);
-
+createApples()
+createOrange()
+createRed()
   drawSprites();
   
 }
+
+
+function createApples() {
+  apple = createSprite(random(50,350),40,10,10);
+  apple.addImage(appleImg);
+  apple.scale=0.07;
+  apple.velocityY = 3;
+  apple.lifetime = 150;
+}
+
+
+function createOrange() {
+  orageL = createSprite(random(50,350),40,10,10);
+  orageL .addImage(orangeImg);
+  orageL .scale=0.07;
+  orageL .velocityY = 3;
+  orageL .lifetime = 150;
+}
+
+
+function createRed() {
+  redL = createSprite(random(50,350),40,10,10);
+  redL.addImage(redImg);
+  redL.scale=0.07;
+  redL.velocityY = 3;
+  redL.lifetime = 150;
+}
+
+var select_sprites = Math.round(random(1,3));
+
+if (frameCount % 80 == 0) {
+  if (select_sprites == 1) {
+    createApples();
+  } else if(select_sprites == 2) {
+    createOrange();
+  } else {
+    createRed();
+  }
+}
+
+rabbit.x = wold.mouseX;
